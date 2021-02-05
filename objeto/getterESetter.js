ef71f07f7296f8a317960f6c5a4280b9c6f975d1
MatheusCostaLara99 <matheuscostalara@hotmail.com>
2021-02-04 23:16:19 -0300

    04-02-2021

--- objeto/getterESetter.js
+++ objeto/getterESetter.js
@@ -1,5 +1,11 @@
 const sequencia = {
     _valor: 1, //convenção
     get valor() {return this._valor++},
-    set valor(valor)
+    set valor(valor){
+        if(valor > this._valor){
+            this._valor = valor
+        }
+    }
 }
+
+console.log(sequencia.valor, sequencia.valor)
